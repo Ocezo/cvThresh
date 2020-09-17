66adb8c24cb67c728a44395438c50f8cd834b19f
Ocezo <jm@ocezo.fr>
2020-09-17 16:41:16 +0200

    Canny trackbar & Eclipse project
    . new trackbars for threshold_canny_value_1 & 2
    . new callback for the Canny window
    . new Eclipse framework glue ~> .gitignore

--- cvThresh.cpp
+++ cvThresh.cpp
@@ -12,16 +12,22 @@ using namespace std;
 int threshold_value = 80;
 int const max_value = 255;
 
+int threshold_canny_value_1 = 50;
+int threshold_canny_value_2 = 100;
+int const max_canny_value = 100;
+
 Mat camFrame, grayFrame, binFrame, cannyFrame;
 
 const char* threshed_window = "Binary";
+const char* canny_window    = "Canny";
 
 /// Function headers
 void Threshold_Demo( int, void* );
+void Canny_Demo( int, void* );
 
 int main() {
     VideoCapture stream1(0);   // 0 is the id of video device.0 if you have only one camera.
-    if (!stream1.isOpened()) { // check if video device has been initialised
+    if (!stream1.isOpened()) { // check if video device has been initialized
         cout << "cannot open camera";
     }
 
@@ -35,8 +41,9 @@ int main() {
         createTrackbar( "Threshold: ", threshed_window, &threshold_value, max_value, Threshold_Demo);
         Threshold_Demo( 0, 0 );
 
-        Canny( camFrame, cannyFrame, 50, 100, 3, true );
-        imshow("Canny", cannyFrame);
+        createTrackbar( "Threshold 1: ", canny_window, &threshold_canny_value_1, max_canny_value, Canny_Demo);
+        createTrackbar( "Threshold 2: ", canny_window, &threshold_canny_value_2, max_canny_value, Canny_Demo);
+        Canny_Demo( 0, 0 );
 
         if (waitKey(30) >= 0 )
             break;
@@ -49,4 +56,10 @@ void Threshold_Demo( int, void* )
 {
     threshold( grayFrame, binFrame, threshold_value, max_value, THRESH_BINARY );
     imshow( threshed_window, binFrame );
+}
+
+void Canny_Demo( int, void* )
+{
+	Canny( grayFrame, cannyFrame, threshold_canny_value_1, threshold_canny_value_2, 3, true );
+	imshow("Canny", cannyFrame);
 }
